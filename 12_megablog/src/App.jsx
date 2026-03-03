9d80faa913d90096e4ef9da25c5cb35b1cd86d09
Varni1102 <varnikumar1512@gmail.com>
2026-03-03 12:16:18 +0530

    React Mega Project - Phase1

---
+++ 12_megablog/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  console.log(import.meta.env.VITE_NAME);
+  
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default App
+ 
